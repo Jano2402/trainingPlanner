32ef570191a1eb29f802bc1d9690154f0033daa4
Jano2402 <martinezalejandrouy7@gmail.com>
2026-06-28 23:47:30 -0300

    Modifying index.css and adding postcss.config.js

---
+++ frontend/postcss.config.js
@@ -0,0 +1,8 @@
+import autoprefixer from "autoprefixer";
+
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {}, // Remove whenever you move the config to the hands of vite
+    autoprefixer: {},
+  },
+};
