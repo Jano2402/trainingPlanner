c622e58b2275f12cd4f8de5fe0b2949bc6beac13
Jano2402 <martinezalejandrouy7@gmail.com>
2026-06-30 20:59:58 -0300

    The project had to be restructured for next.js development

--- frontend/src/App.tsx
+++
@@ -1,3 +0,0 @@
-export function App() {
-  return <h1 className="text-red-600">Hello</h1>;
-}
--- frontend/src/utils/sum.ts
+++
@@ -1,9 +0,0 @@
-type Numbs = {
-  a: number;
-  b: number;
-};
-
-export function sum(a: number, b: number) {
-  let res: number = a + b;
-  return res;
-}
